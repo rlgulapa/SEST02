2c33cf49f4f32a952272bac9d2ce041b27155b1a
rlgulapa <rlgulapa@gmail.com>
2025-01-08 21:59:51 +0800

    NPM and Express Day 2 and uncompleted assigment

--- 14 - NPM and Express/shopping-cart-api/app.js
+++ 14 - NPM and Express/shopping-cart-api/app.js
@@ -4,6 +4,9 @@ const port = 3000;
 const products = require("./products");
 const cartItems = require("./cart");
 
+// Middleware
+app.use(express.json());
+
 // app.get()
 // app.post()
 // app.put()
@@ -32,6 +35,105 @@ app.get("/products/:productID", (request, response) => {
     response.json(product);
   } else {
     // Return an error and tell the user the product is not found.
+    response.status(404).json({ message: "Product not found" });
+  }
+});
+
+function generateUniqueId() {
+  if (products.length === 0) {
+    // No product object inside of the products array.
+    return 1;
+  }
+  const lastProductObject = products[products.length - 1];
+  return lastProductObject.id + 1;
+}
+
+// .post(Route/URL, Callback(Route Handler))
+app.post("/products", (request, response) => {
+  // This will return the same value as desctructuring objects.
+  // const name = request.body.name
+  // const price = request.body.price
+  const { name, price } = request.body;
+  if (!name || !price) {
+    return response.status(400).json({ message: "Name and price is required" });
+  }
+  const newProduct = {
+    id: generateUniqueId(),
+    name,
+    price,
+  };
+  products.push(newProduct);
+  response.status(201).json({ message: "Product added to the product list." });
+});
+
+app.put("/products/:productId", (request, response) => {
+  const productId = parseInt(request.params.productId);
+  const { name, price } = request.body;
+  if (!name || !price) {
+    return response.status(400).json({ message: "Name and price is required" });
+  }
+  const product = products.find(
+    (productObject) => productObject.id === productId
+  );
+  if (product) {
+    product.name = name;
+    product.price = price;
+    response.status(200).json({ message: "Product updated successfully." });
+  } else {
+    response.status(404).json({ message: "Product not found" });
+  }
+});
+
+app.delete("/products/:productId", (request, response) => {
+  const productId = parseInt(request.params.productId);
+
+  const productIndex = products.findIndex(
+    (productObject) => productObject.id === productId
+  );
+  if (productIndex !== -1) {
+    // .splice(start, deleteCount?, element/s)
+    products.splice(productIndex, 1);
+    response.status(200).json({ message: "Product deleted successfully" });
+  } else {
+    response.status(404).json({ message: "Product not found" });
+  }
+});
+
+// cart
+
+// 1. Add a new product Object to the cartItems array.
+// 2. Delete a product Object from the cartItems array.
+app.get("/cart", (request, response) => {
+  // Status Code 200: Successful
+  response.status(200).json(cartItems);
+});
+
+app.post("/cart", (request, response) => {
+  // This will return the same value as desctructuring objects.
+  // const name = request.body.name
+  // const price = request.body.price
+  const { product } = request.body;
+  if (!product) {
+    return response.status(400).json({ message: "Product object is required" });
+  }
+  cartItems.push(product);
+  response.status(201).json({ message: "Product added to the Cart Items." });
+});
+
+app.delete("/cart/:productId", (request, response) => {
+  const productId = parseInt(request.params.productId);
+
+  // TODO
+  // const productIndex = products.findIndex(
+  //   (productObject) => productObject.id === productId
+  // );
+  if (productIndex !== -1) {
+    // .splice(start, deleteCount?, element/s)
+    products.splice(productIndex, 1);
+    response
+      .status(200)
+      .json({ message: "Product deleted from cart items successfully" });
+  } else {
     response.status(404).json({ message: "Product not found" });
   }
 });
